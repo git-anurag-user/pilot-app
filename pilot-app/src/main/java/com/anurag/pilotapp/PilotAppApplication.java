a682b4267b01ee02e0024384f31ff0ff6b7cf20e
Anurag Jauhari <anurag.jauhari@ericsson.com>
2021-08-24 13:59:13 +0530

    initial project commit

---
+++ pilot-app/src/main/java/com/anurag/pilotapp/PilotAppApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.pilotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotAppApplication.class, args);
+	}
+
+}
---
+++ pilot-app/src/test/java/com/anurag/pilotapp/PilotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.pilotapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
